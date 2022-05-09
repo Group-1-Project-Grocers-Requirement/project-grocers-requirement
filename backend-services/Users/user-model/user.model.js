83387942b565c93eb4dc7d4dd770494ca039aa3f
Bhalchandra Rane <brane6963@gmail.com>
2022-05-09 15:53:30 +0530

    may 9

--- backend-services/Users/user-model/user.model.js
+++ backend-services/Users/user-model/user.model.js
@@ -1,4 +1,5 @@
 let mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 mongoose.Promise = global.Promise;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 let UserSchema = mongoose.Schema({
@@ -14,6 +15,25 @@ let UserSchema = mongoose.Schema({
     locked:Boolean
 })
 UserSchema.plugin(AutoIncrement , {id: 'user_id_counter', inc_field: '_id'});
+// UserSchema.pre("save", function (next) {
+//   const user = this
+//   if (this.isModified("pass") || this.isNew) {
+//     bcrypt.genSalt(10, function (saltError, salt) {
+//       if (saltError) {
+//         return next(saltError)
+//       } else {
+//         bcrypt.hash(user.pass, salt, function(hashError, hash) {
+//           if (hashError) {
+//             return next(hashError)
+//           }
+//           user.pass = hash
+//           next()
+//         })
+//       }
+//     })
+//   } else {
+//     return next() 
+//   }
+// })
 let UserModel = mongoose.model("user",UserSchema,"User");
-
 module.exports = UserModel;
